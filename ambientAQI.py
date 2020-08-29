2e6336f1f369deff7fdacd6103acc9ac948f7fcb
joeyverbeke <joeyverbeke@gmail.com>
2020-08-29 13:30:02 -0700

    change to non-thingspeak json, restructuring

--- ambientAQI.py
+++ ambientAQI.py
@@ -1,58 +1,93 @@
 #!/usr/bin/env python
-import urllib2,json
+import urllib2, json, opc, time
+import numpy as np
+
+#thingspeak
 READ_API_KEY='OZUQH57IOWW840PA'
 CHANNEL_ID='1103672'
 
-import opc, time
+#json
+ID='60015'
+KEY='OZUQH57IOWW840PA'
+
 numLEDs = 16
 
 client = opc.Client('localhost:7890')
 
-good_low = 		[ (0,255,0) ]  		#AQI 0-24
-good_high = 	[ (127,255,0) ] 	#AQI 25-50
-moderate_low = 	[ (255,255,0) ] 	#AQI 51-75
-moderate_high = [ (255,200,0) ] 	#AQI 76-100
-UFSI_low = 		[ (255,127,0) ] 	#AQI 101-125
-UFSI_high = 	[ (255,75,0) ] 		#AQI 126-150
-unhealthy = 	[ (255,0,0) ] 		#AQI 151+
+###Stepwise###
+#good_low = 		[ (0,255,0) ]  		#AQI 0-24
+#good_high = 	[ (127,255,0) ] 	#AQI 25-50
+#moderate_low = 	[ (255,255,0) ] 	#AQI 51-75n
+#moderate_high = [ (255,200,0) ] 	#AQI 76-100
+#UFSI_low = 		[ (255,127,0) ] 	#AQI 101-125
+#UFSI_high = 	[ (255,75,0) ] 		#AQI 126-150
+#unhealthy = 	[ (255,0,0) ] 		#AQI 151+
 
-pm = 0
-ledColor = good_low
+pm = pm_10min = 0
+ledColor = [ (0,0,0) ]
 
 brightness = 1
 
-while True:
-    conn = urllib2.urlopen("http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
-                           % (CHANNEL_ID,READ_API_KEY))
+#def fadeToColor(oldColor, newColor):
+#    colorDifference = np.subtract(newColor, oldColor)
+#    index = np.nonzero(colorDifference)
+#    colorFader = [[oldColor[0]]
+#
+#    for i in range(colorDifference[index])
+#        oldColor[index] += 1
+#        time.sleep(1/30.0)
 
-    response = conn.read()
-    print "http status code=%s" % (conn.getcode())
-    data=json.loads(response)
-    print data['field8'],data['created_at']
-    conn.close()
+def update():
+    while True:
+        conn = urllib2.urlopen("https://www.purpleair.com/json?key=%s&show=%s" \
+                               % (KEY,ID))
 
-    pm = float(data['field8'])
+        response = conn.read()
+        print "http status code=%s" % (conn.getcode())
+        data=json.loads(response)
+        conn.close()
 
-    if time.strftime("%H", time.localtime()) >= 22 or time.strftime("%H", time.localtime()) < 1:
-        brightness = 0.5
-    elif time.strftime("%H", time.localtime()) > 1 and time.strftime("%H", time.localtime()) < 8:
-        brightness = 0
-    else:
-        brightness = 1
+        stats = json.loads(data['results'][0]['Stats']) #get PM2.5 values from json
 
-    if pm <= 12:    #good
-        ledColor = [ (int(pm * 21 * brightness), int(255 * brightness), 0) ]
-    elif pm <= 35:  #moderate
-    	ledColor = [ (int(255 * brightness), int( (255 - int((pm - 12) * 5.65)) * brightness ), 0) ]
-    elif pm <= 55:  #unhealthy for sensitive individuals
-    	ledColor = [ (int(255 * brightness), int( (127 - int((pm - 35) * 6.35)) * brightness ), 0) ]
-    elif pm <= 150: #unhealthy
-        ledColor = [ (int(255 * brightness), 0, int((pm - 55) * 2.68 * brightness)) ]
-    else:           #very unhealthy
-        ledColor = [ (int(255 * brightness), 0, int(255 * brightness)) ]
+        pm = float(stats['v1'])
+        pm_10min = float(stats['v2'])
 
+        if int(time.strftime("%H", time.localtime())) >= 22:
+            brightness = 0.5
+        elif int(time.strftime("%H", time.localtime())) < 8:
+            brightness = 0.2
+        else:
+            brightness = 1
+
+        if pm <= 12:    #good
+            ledColor = [ (int(pm * 21 * brightness), int(255 * brightness), 0) ]
+        elif pm <= 35:  #moderate
+        	ledColor = [ (int(255 * brightness), int( (255 - (pm - 12) * 5.65) * brightness ), 0) ]
+        elif pm <= 55:  #unhealthy for sensitive individuals
+        	ledColor = [ (int(255 * brightness), int( (127 - (pm - 35) * 6.35) * brightness ), 0) ]
+        elif pm <= 150: #unhealthy
+            ledColor = [ (int(255 * brightness), 0, int((pm - 55) * 2.68 * brightness)) ]
+        else:           #very unhealthy
+            ledColor = [ (int(255 * brightness), 0, int(255 * brightness)) ]
+
+        for i in range(numLEDs):
+        	pixels = ledColor * numLEDs
+        	client.put_pixels(pixels)
+
+        print 'ledColor: ' + str(ledColor)
+
+        time.sleep(120)
+
+def cleanup():
     for i in range(numLEDs):
-    	pixels = ledColor * numLEDs
-    	client.put_pixels(pixels)
+        pixels = [(0,0,0)] * numLEDs
+        client.put_pixels(pixels)
 
-    time.sleep(60)
+def main():
+    try:
+        update()
+    finally:
+        cleanup()
+
+if __name__=='__main__':
+    main()
